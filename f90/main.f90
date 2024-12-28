c640759a528a3f52c7e7ae14593b6b49a4088d00
Shiro Hirano <46644629+ShiroHirano@users.noreply.github.com>
2024-12-28 17:40:44 +0900

    Update main.f90

--- f90/main.f90
+++ f90/main.f90
@@ -1,9 +1,10 @@
 program main
     use m_stochasticSTF, only : kd, StochasticSTF
     implicit none
-    real(kd),allocatable :: STF(:) ! Source Time Function to be obtained
-    integer,parameter :: n = 1000  ! length of STF (> 30 is recommended)
-    real(kd),parameter :: r = 1e0  ! roughness of STF (1.0 for omega-seuqre)
+    real(kd), allocatable :: STF(:)   ! Source Time Function to be obtained
+    integer,  parameter   :: n = 1000 ! length of STF (> 30 is recommended)
+    real(kd), parameter   :: r = 1e0  ! roughness of STF (optional; default value is 1.0, resulting in omega-square model)
+    integer,  parameter   :: d = 2    ! dimension of Bessel bridege (optional; default value is 2. larger it is, the rougher STF is)
     STF = StochasticSTF(n,r)
     open(00,file="STF.txt",action="write",status="replace")
         write(00,'(1G0)') STF
