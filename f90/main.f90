353a9717ec1a05bf51331e0ec3ef95fd1d04d0e5
Shiro Hirano <46644629+ShiroHirano@users.noreply.github.com>
2024-12-29 20:38:12 +0900

    Update main.f90
    output results as a svg file

--- f90/main.f90
+++ f90/main.f90
@@ -1,12 +1,17 @@
 program main
-    use m_stochasticSTF, only : kd, StochasticSTF
+    use m_stochasticSTF, only : kd, StochasticSTF, output_svg_path
     implicit none
-    real(kd), allocatable :: STF(:)   ! Source Time Function to be obtained
-    integer,  parameter   :: n = 1000 ! length of STF (> 30 is recommended)
-    real(kd), parameter   :: r = 1e0  ! roughness of STF (optional; default value is 1.0, resulting in omega-square model)
-    integer,  parameter   :: d = 2    ! dimension of Bessel bridege (optional; default value is 2. larger it is, the rougher STF is)
+    real(kd), allocatable :: STF(:)   ! Source Time Function to be obtained.
+    integer,  parameter   :: n = 1000 ! Length of STF (> 30 is recommended).
+    real(kd), parameter   :: r = 1e0  ! (Optional. Ratio of two corner frequencies;
+                                      ! default value is 1.0, 
+                                      ! resulting in omega-square model).
+!    integer,  parameter   :: d = 2    ! (Optional. Dimension of Bessel bridege;
+                                      ! default value is 2.
+                                      ! Larger it is, the smoother STF is).
     STF = StochasticSTF(n,r)
-    open(00,file="STF.txt",action="write",status="replace")
-        write(00,'(1G0)') STF
-    close(00)
+!    STF = StochasticSTF(n,r,d)
+    call output_svg_path(STF) ! The result is output as STF.svg.
+                              ! Ignore the first and last line
+                              ! of the file to get numerics.
 end program main
